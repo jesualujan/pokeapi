d695d31e34323b6fffd6894038695098950058b6
jesualujan <lujan.oficial@gmail.com>
2024-03-11 17:10:02 -0600

    ✨ creación del proyecto

--- pokeapi.js
+++ pokeapi.js
@@ -1,19 +1,176 @@
-const morgan = require('morgan')
+const pokeContent = document.getElementById('pokemonContent');
+let pokeForm = document.getElementById('searchPokemon');
+let generationshow = 1
+const modalSearch = document.getElementById('pokemonContent')
+const divGeneration = document.getElementById('textGen')
+/*ordenar xr generacion*/
+/*Primera Gen 1-151*/
+/*Segunda Gen 152-251*/
+/*tercera Gen 252-386*/
 
-const logger = morgan('dev')
+function showPokemonGen(gen){
+    const pokemonGen = {
+        1:[1, 151],
+        2:[152,251],
+        3:[252, 386]
+    };
 
-const URI = 'https://pokeapi.co/api/v2/pokemon/25'
+    const pokemonGenDefault = [1, 151];
+    const generacion = pokemonGen[gen] || pokemonGenDefault;
+    return generacion;
+    
+}
 
-// Función para realizar una petición a la PokeAPI
-// tenemos una función asincrona,
-const getPokemon = async () => {
-      await fetch(URI) // tenemos que indicar en una función asincrona que va a esperar 
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error:', error));
-};
-
-// Envolver la función getPokemon con el logger de Morgan
-logger({}, {},  getPokemon)
+let pokemonGeneration = showPokemonGen(generationshow)
 
 
+/*cambiar de generacion*/
+
+let arrowRight = document.getElementById('arrow-right').addEventListener('click', e=>{
+   
+    if (generationshow < 4){
+        modalSearch.innerHTML = '';
+       generationshow += 1
+       pokemonGeneration = showPokemonGen(generationshow)
+       divGeneration.innerHTML = 'Gen ' + generationshow
+       drawPokemon()
+   }
+})
+
+
+let arrowleft = document.getElementById('arrow-left').addEventListener('click', e=>{
+    
+     if (generationshow > 0){
+        modalSearch.innerHTML = '';
+        generationshow -= 1
+        pokemonGeneration = showPokemonGen(generationshow)
+        divGeneration.innerHTML = 'Gen ' + generationshow
+        drawPokemon()
+        console.log(generationshow)
+    }
+ })
+
+
+const drawPokemon = async () =>{
+    for (let i = pokemonGeneration[0]; i <= pokemonGeneration[1]; i++) {
+		await getPokemons(i);
+	}
+}
+
+const getPokemons = async (id, modal) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+    
+    try {
+        const rest = await fetch(url);
+        
+        if (!rest.ok) {
+            alert('Pokemon not found. Please try again.')
+        }
+        
+        const pokemon = await rest.json();
+        createPokemon(pokemon, modal);
+    } catch (error) {
+        console.error('Error:', error.message);
+        
+        if (modal) {
+            // Mostrar un mensaje de error en el modal
+            showModal('Pokemon not found. Please try again.');
+        } else {
+            // Mostrar un mensaje de error en la consola o alerta
+            alert('Pokemon not found. Please try again.');
+        }
+    }
+}
+
+
+/*pintar card pokemon*/
+const colors = {
+    fire: '#FFA05D',
+	grass: '#8FD594',
+	electric: '#FFE43B',
+	water: '#7E97C0',
+	ground: '#CAAC4D',
+	rock: '#90642D',
+	poison: '#9D5B9B',
+	bug: '#EAFD71',
+	dragon: '#97b3e6',
+	psychic: '#FF96B5',
+	flying: '#CDCDCD',
+	fighting: '#FF5D5D',
+	normal: '#FFFFFF'
+}
+
+const main_types = Object.keys(colors)
+
+
+function  createPokemon(pokemon, modal){
+    const pokemonEl = document.createElement('div');
+    
+	pokemonEl.classList.add('pokemon');
+    
+	const poke_types = pokemon.types.map(type => type.type.name);
+	const type = main_types.find(type => poke_types.indexOf(type) > -1);
+	const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+	const color = colors[type];
+	
+	pokemonEl.style.backgroundColor = color;
+
+	
+
+	if (modal !==true){
+        const pokeInnerHTML = `
+        <div class="img-container">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="${name}" />
+        </div>
+        <div class="info">
+            <span class="number">#${pokemon.id
+							.toString()
+							.padStart(3, '0')}</span>
+            <h3 class="name">${name}</h3>
+            <small class="type">Tipo: <span>${type}</span></small>
+        </div>
+    `;
+        pokemonEl.innerHTML = pokeInnerHTML;
+        pokeContent.appendChild(pokemonEl);
+    }
+
+    else{
+        const pokeInnerHTML = `
+        <div class="modal" id="modalPokemon">
+        <div class="pokemon">
+        <div class="img-container">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="${name}" />
+        </div>
+        <div class="info">
+            <span class="number">#${pokemon.id
+							.toString()
+							.padStart(3, '0')}</span>
+            <h3 class="name">${name}</h3>
+            <small class="type">Tipo: <span>${type}</span></small>
+        </div>
+        </div>
+    
+    </div>`;
+                
+
+    modalSearch.innerHTML = pokeInnerHTML;
+        
+    }
+}
+
+drawPokemon()
+
+
+/*Buscar pokemon*/
+
+pokeForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    let searchPokemon = document.getElementById('pokemon').value;
+    getPokemons(searchPokemon, true);
+})
+
+function exitModal(){
+   const modalPokemon = document.getElementById('modalPokemon');
+   modalPokemon.style.display ='none'
+   drawPokemon()
+}
